94e80cf4bea573101fd0f9839c4990f25d81b29a
thistonyb <this.tonybaker@gmail.com>
2020-03-13 23:16:04 -0700

    Done.

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -5,20 +5,28 @@ class Pet extends React.Component {
     let disabledButton = (
       <button className="ui disabled button">Already adopted</button>
     );
-    let adoptButton = <button className="ui primary button">Adopt pet</button>;
+    let adoptButton = (
+      <button
+        onClick={() => this.props.onAdoptPet(this.props.pet.id)}
+        className="ui primary button"
+      >
+        Adopt pet
+      </button>
+    );
     return (
       <div className="card">
         <div className="content">
           <a className="header">
             {/*'♀' OR '♂' */}
             {this.props.pet.name}
+            {this.props.pet.gender === "female" ? "♀" : "♂"}
           </a>
           <div className="meta">
             <span className="date">{this.props.pet.type}</span>
           </div>
           <div className="description">
             <p>Age: {this.props.pet.age}</p>
-            <p>Weight: {this.props.pet.age} </p>
+            <p>Weight: {this.props.pet.weight} </p>
           </div>
         </div>
         <div className="extra content">
